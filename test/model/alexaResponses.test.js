b9eec8d828ed2bdd675030801dfad1026277aa95
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2018-11-29 15:05:51 +0000

    Adding an empty set of unit tests for alexResponses.

---
+++ test/model/alexaResponses.test.js
@@ -0,0 +1,8 @@
+describe('Alexa Responses', async () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it ('alexa responses should be covered by myBuses API unit tests', async () => {
+    });
+});
